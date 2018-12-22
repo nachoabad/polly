7ef19cb78a7306b3708faf1260a62d8b776d6e2d
Nacho Abad <liao2512@gmail.com>
2018-12-22 23:13:54 +0000

    adds handle click on answer

--- app/javascript/packs/poll.jsx
+++ app/javascript/packs/poll.jsx
@@ -5,13 +5,25 @@ import PropTypes from 'prop-types'
 class Poll extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {question: new Date()};
+    this.state = {
+      answers: [],
+      currentQuestion: 0
+    };
+  }
+  
+  componentDidMount() {
+    const data = {"id":1,"title":"Conferencia Volkwageros","slug":"vw","user_id":1,"questions":[{"id":1,"sentence":"¿Cómo te pareció el evento?","position":1,"poll_id":1,"choices":[{"id":1,"sentence":"Excelente","position":1,"question_id":1},{"id":2,"sentence":"Normal","position":2,"question_id":1},{"id":3,"sentence":"Pésimo","position":3,"question_id":1}]},{"id":2,"sentence":"El precio fue...","position":2,"poll_id":1,"choices":[{"id":4,"sentence":"Barato","position":1,"question_id":2},{"id":5,"sentence":"Justo","position":2,"question_id":2},{"id":6,"sentence":"Caro","position":3,"question_id":2}]},{"id":3,"sentence":"¿Volverías el año próximo?","position":3,"poll_id":1,"choices":[{"id":7,"sentence":"Seguro","position":1,"question_id":3},{"id":8,"sentence":"No sé","position":2,"question_id":3},{"id":9,"sentence":"No","position":3,"question_id":3}]}]}
+    
+    this.setState({
+      poll: data,
+      question: data.questions[0].sentence
+    });
   }
   
   render() {
     return (
       <div>
-        <Question sentence={this.props.question} />
+        <Question sentence={this.state.question} />
         <div className="row">
           <Choice color="success" sentence={this.props.choice1} />
           <Choice color="neutral" sentence={this.props.choice2} />
@@ -31,10 +43,21 @@ class Question extends React.Component {
 }
 
 class Choice extends React.Component {
+  constructor(props) {
+    super(props);
+   
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick(id) {
+    console.log('The link was clicked.' + id);
+  }
+  
   render() {
     return (
       <div className="col-12 text-center">
-        <button className={`btn btn-${this.props.color} btn-lg`}>
+        <button className={`btn btn-${this.props.color} btn-lg`}
+                onClick={this.handleClick.bind(this, 99)}>
           {this.props.sentence}
         </button>
       </div>
@@ -52,8 +75,7 @@ Poll.propTypes = {
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Poll question="¿Qué te pareció el sonido del concierto?"
-          choice1="EXCELENTE"
+    <Poll choice1="EXCELENTE"
           choice2="REGULAR"
           choice3="PÉSIMO"/>,
     document.getElementById('react-poll')
