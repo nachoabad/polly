540ac28a7363f93117fb34e5fead162506a3b10f
Nacho Abad <liao2512@gmail.com>
2019-03-17 21:35:48 +0000

    adds answer counting and color Choice

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,6 +1,25 @@
 class Choice < ApplicationRecord
   belongs_to :question
   has_many :answers
-  
+
   default_scope { order(position: :asc) }
+
+  def total
+    answers.count
+  end
+
+  def percentage
+    100 * total / question.total_answers
+  end
+
+  def color
+    case position
+    when 1
+      'text-success'
+    when 2
+      'text-white'
+    when 3
+      'text-warning'
+    end
+  end
 end
