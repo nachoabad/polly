0fca274a3cfe514a8ddea3b74137a6a68163ef8c
Nacho Abad <liao2512@gmail.com>
2019-03-17 21:33:27 +0000

    adds #total_answers to Question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,11 @@
 class Question < ApplicationRecord
   belongs_to :poll
   has_many :choices
+  has_many :answers, through: :choices
   
   default_scope { order(position: :asc) }
+
+  def total_answers
+    answers.count
+  end
 end
